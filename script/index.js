31a7f170e7954c1024db4f621477ca57040255c7
TariqulIslamTusher <islamtariqul652@gmail.com>
2023-02-18 12:00:17 +0700

    completed validation check

--- script/index.js
+++ script/index.js
@@ -5,10 +5,13 @@ document.getElementById('triangle-card-btn').addEventListener('click', function 
     const name = getInnerTextInString('name1');
     const heigth = getInnerValueById('triangleInput1')
     const base = getInnerValueById('triangleInput2')
-    const area = 0.5 * heigth * base
-    makeTheTableData(serial, name, area.toFixed(2))
-    return alert('Please enter a valid Number')
 
+    if (isNaN(heigth) || heigth <= 0 || isNaN(base) || base <= 0) {
+        return alert('Please enter a valid Number')
+    } else{
+        const area = 0.5 * heigth * base
+        makeTheTableData(serial, name, area.toFixed(2))
+    }
 })
 
 //ractangle-card-btn event
@@ -17,9 +20,12 @@ document.getElementById('ractangle-card-btn').addEventListener('click', function
     const name = getInnerTextInString('name2');
     const width = getInnerValueById('ractangleInput1')
     const length = getInnerValueById('ractangleInput2')
-    const area = width * length;
-
-    makeTheTableData(serial, name, area.toFixed(2))
+    if(isNaN(width) || width <= 0 || isNaN(length) || length <= 0){
+        return alert('Please enter a valid Number')
+    } else {
+        const area = width * length;
+        makeTheTableData(serial, name, area.toFixed(2))
+    }
 })
 
 //parallelogram-card-btn event
@@ -37,11 +43,15 @@ document.getElementById('parallelogram-card-btn').addEventListener('click', func
 document.getElementById('rhombus-card-btn').addEventListener('click', function () {
     serial += 1;
     const name = getInnerTextInString('name4');
-    const d1 = getInnerTextInNumberById('rhombus-d1')
-    const d2 = getInnerTextInNumberById('rhombus-d2')
-    const area = 0.5 * d1 * d2;
-
-    makeTheTableData(serial, name, area.toFixed(2))
+    const d1 = getInnerValueById('rhombus-d1')
+    const d2 = getInnerValueById('rhombus-d2')
+    if(isNaN(d1) || d1 <= 0 || isNaN(d2) || d2 <= 0){
+        return alert('Please is a valid number')
+    } else {
+        const area = 0.5 * d1 * d2;
+    
+        makeTheTableData(serial, name, area.toFixed(2))
+    }
 })
 
 //pentagon-card-btn event
